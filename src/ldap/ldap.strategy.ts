4cf3433c1af8e49f60f43822114e5c455fafd92c
F1uke <26509248+lzy3me@users.noreply.github.com>
2023-10-05 10:39:35 +0700

    passReqToCallback

--- src/ldap/ldap.strategy.ts
+++ src/ldap/ldap.strategy.ts
@@ -2,12 +2,11 @@ import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import * as Strategy from "passport-ldapauth";
 
-console.log(process.env.LDAP_ADDR);
-
 @Injectable()
 export class LdapStrategy extends PassportStrategy(Strategy, "ldap") {
   constructor() {
     super({
+      passReqToCallback: true,
       server: {
         url: process.env.LDAP_ADDR,
         bindDN: process.env.BIND_DN,
